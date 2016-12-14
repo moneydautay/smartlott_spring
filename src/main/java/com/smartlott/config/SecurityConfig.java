21a8dabc5451f9932e72817370761f7202228aca
Nguyễn Mai Lam <nguyenlamit86@gmail.com>
2016-12-14 09:06:49 +0700

    Added folder and security config

---
+++ src/main/java/com/smartlott/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.smartlott.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by greenlucky on 12/13/16.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    /**
+     * The key enscryption password
+     */
+    private static final String SALT = "wjfaj9*($*#_$)#($)#$#$";
+
+    #@Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }
+
+    /** PUBLIC URLS */
+    private static final String[] PUBLIC_MATCHES={
+            "/webjars/**",
+            "/css/**",
+            "/js/**",
+            "images/**",
+            "/",
+            "console/**",
+            "error/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+        List<String> activeProflies = Arrays.asList(en)
+
+    }
+
+
+
+}
